822ad30b42dacba5ac5bb7807ecf322db16e1c67
Затолокин Андрей <andrey.gbr@mail.ru>
2024-02-27 17:38:43 +0300

    Добавил проект с ботом

---
+++ SmartSchoolboyBot/SmartSchoolboyBot/Program.cs
@@ -0,0 +1,7 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+

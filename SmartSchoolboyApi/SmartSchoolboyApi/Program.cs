2ddd55ee004d4c62d475398d16ee9183899bc351
Затолокин Андрей <andrey.gbr@mail.ru>
2024-05-23 00:02:58 +0300

    увше

--- SmartSchoolboyApi/SmartSchoolboyApi/Program.cs
+++ SmartSchoolboyApi/SmartSchoolboyApi/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SmartSchoolboyBaseContext>(option =>
-    option.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    option.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 

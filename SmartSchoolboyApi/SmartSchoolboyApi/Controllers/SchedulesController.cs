e82dd3157f006df2f604975cad11e2c968a053ef
Затолокин Андрей <andrey.gbr@mail.ru>
2024-05-25 01:59:22 +0300

    Модифицировал контроллер Schedules

--- SmartSchoolboyApi/SmartSchoolboyApi/Controllers/SchedulesController.cs
+++ SmartSchoolboyApi/SmartSchoolboyApi/Controllers/SchedulesController.cs
@@ -15,58 +15,117 @@ namespace SmartSchoolboyApi.Controllers
             _context = context;
         }
 
-        // GET: api/Schedules
+        /// <summary>
+        /// GET: api/Schedules
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Schedule>>> GetSchedules()
         {
-          if (_context.Schedules is null)
-              return NotFound();
+            try
+            {
+                if (_context.Schedules is null)
+                    return NotFound();
 
-            return await _context.Schedules.ToListAsync();
+                return Ok(await _context.Schedules.ToListAsync());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, the server is not responding");
+            }
         }
 
-        // GET: api/Schedules/5
+        /// <summary>
+        /// GET: api/Schedules/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<Schedule>> GetSchedule(int id)
         {
-          if (_context.Schedules is null)
-              return NotFound();
+            try
+            {
+                if (_context.Schedules is null)
+                    return NotFound();
 
-            var schedule = await _context.Schedules.FindAsync(id);
+                var schedule = await _context.Schedules.FindAsync(id);
 
-            if (schedule is null)
-                return NotFound();
+                if (schedule is null)
+                    return NotFound();
 
-            return schedule;
+                return Ok(schedule);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, the server is not responding");
+            }
         }
 
-        // PUT: api/Schedules/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// GET: api/Schedules/search
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("search/{search}")]
+        public async Task<ActionResult<Schedule>> SearchSchedule(string search)
+        {
+            try
+            {
+                if (_context.Schedules is null)
+                    return NotFound();
+
+                var schedule = await _context.Schedules.Where(p => p.Group.Name.ToLower().Trim().Contains(search.ToLower().Trim()) ||
+                    p.Group.Course.Name.ToLower().Trim().Contains(search.ToLower().Trim())).ToListAsync();
+
+                return Ok(schedule);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, the server is not responding");
+            }
+        }
+
+        /// <summary>
+        /// PUT: api/Schedules/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="schedule"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> PutSchedule(int id, Schedule schedule)
         {
-            if (id != schedule.Id)
-                return BadRequest();
-
-            _context.Entry(schedule).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ScheduleExists(id))
-                    return NotFound();
-                else
-                    throw;
-            }
+                if (id != schedule.Id)
+                    return BadRequest();
 
-            return NoContent();
+                _context.Entry(schedule).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleExists(id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, the server is not responding");
+            }
         }
 
-        // POST: api/Schedules
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// POST: api/Schedules
+        /// </summary>
+        /// <param name="schedule"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<ActionResult<Schedule>> PostSchedule(Schedule schedule)
         {
@@ -89,13 +148,17 @@ namespace SmartSchoolboyApi.Controllers
 
                 return CreatedAtAction(nameof(PostSchedule), new { id = schedule.Id }, schedule);
             }
-            catch
+            catch (Exception)
             {
-                return BadRequest();
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, the server is not responding");
             }
         }
 
-        // DELETE: api/Schedules/5
+        /// <summary>
+        /// DELETE: api/Schedules/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteSchedule(int id)
         {
@@ -113,9 +176,9 @@ namespace SmartSchoolboyApi.Controllers
 
                 return NoContent();
             }
-            catch
+            catch (Exception)
             {
-                return BadRequest();
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, the server is not responding");
             }
         }
 

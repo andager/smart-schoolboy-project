d6c12b35ec7206f0a944e44cf766aaa6761a2ed1
Затолокин Андрей <andrey.gbr@mail.ru>
2024-06-03 12:54:17 +0300

    добавил в API сжатие

--- SmartSchoolboyApi/SmartSchoolboyApi/Program.cs
+++ SmartSchoolboyApi/SmartSchoolboyApi/Program.cs
@@ -1,19 +1,42 @@
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using SmartSchoolboyApi.Models;
+using System.IO.Compression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddResponseCompression(option =>
+{
+    option.EnableForHttps = true;
+    option.Providers.Add<BrotliCompressionProvider>();
+    option.Providers.Add<GzipCompressionProvider>();
+});
 
+// Настройка уровня сжатия для Brotli
+builder.Services.Configure<BrotliCompressionProviderOptions>(option =>
+{
+    option.Level = CompressionLevel.Fastest;
+});
+
+
+// Настройка уровня сжатия для Gzip
+builder.Services.Configure<GzipCompressionProviderOptions>(option =>
+{
+    option.Level = CompressionLevel.Fastest;
+});
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SmartSchoolboyBaseContext>(option =>
-    option.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+    option.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("HostingConnection"))); 
 
 var app = builder.Build();
+app.UseResponseCompression();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- SmartSchoolboyApp/SmartSchoolboyApp/MVVM/ViewModel/AddEditGroupViewModel.cs
+++ SmartSchoolboyApp/SmartSchoolboyApp/MVVM/ViewModel/AddEditGroupViewModel.cs
@@ -1,31 +1,93 @@
 ﻿using SmartSchoolboyApp.Classes;
+using SmartSchoolboyApp.Converters;
 using SmartSchoolboyApp.MVVM.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace SmartSchoolboyApp.MVVM.ViewModel
 {
     public class AddEditGroupViewModel : ObservableObject
     {
         #region Fields
-
+        private Group _group;
+        private string _windowName;
+        private bool _isViewVisible = true;
+        private int _indexCourse;
+        private Course _selectedCourse;
+        private List<Course> _courses;
+        private string _courseName;
+        private List<Student> _students;
         #endregion
 
         #region Properties
-
+        public string WindowName
+        {
+            get { return _windowName; }
+            set { _windowName = value; OnPropertyChanged(nameof(WindowName)); }
+        }
+        public bool IsViewVisible
+        {
+            get { return _isViewVisible; }
+            set { _isViewVisible = value; OnPropertyChanged(nameof(IsViewVisible)); }
+        }
+        public int IndexCourse
+        {
+            get { return _indexCourse; }
+            set { _indexCourse = value; OnPropertyChanged(nameof(IndexCourse)); }
+        }
+        public Course SelectedCourse
+        {
+            get { return _selectedCourse; }
+            set { _selectedCourse = value; OnPropertyChanged(nameof(SelectedCourse)); }
+        }
+        public List<Course> Courses
+        {
+            get { return _courses; }
+            set { _courses = value; OnPropertyChanged(nameof(Course)); }
+        }
+        public string CourseName
+        {
+            get { return _courseName; }
+            set { _courseName = value; OnPropertyChanged(nameof(CourseName)); }
+        }
+        public List<Student> Students
+        {
+            get { return _students; }
+            set { _students = value; OnPropertyChanged(nameof(Students)); }
+        }
         #endregion
 
         #region Commands
+        public ICommand UpdateDataCommand { get; }
 
         #endregion
 
         #region Constructor
         public AddEditGroupViewModel(Group group)
         {
+            _group = group;
 
+            if (_group is null)
+                WindowName = "Add Group";
+            else
+            {
+                WindowName = "Edit Group";
+                Students = _group.students;
+                IndexCourse = _group.courseId - 1;
+            }
+
+            UpdateDataCommand = new RelayCommand(ExecuteUpdateDataCommand);
+            ExecuteUpdateDataCommand(null);
+        }
+
+        private async void ExecuteUpdateDataCommand(object obj)
+        {
+            Courses = await App.ApiConnector.GetTAsync<List<Course>>("Courses");
         }
         #endregion
     }
